c9378f9f2272c3ec2b1fe73da3222b4fe4dfadf0
Haim <Haim.Michalashvili@lusha.com>
2024-05-20 00:38:28 +0300

    - add models to the routes parameters api

---
+++ backend/app/models.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel
+
+
+class User(BaseModel):
+    username: str
+    password: str
+
+
+class Track(BaseModel):
+    track_id: str
+    track_name: str
+    artist_name: str
+    year: int
+    relevance_percentage: float
+
+
+class UserTrackRequest(BaseModel):
+    username: str
+    track_id: str
+
+
+class CSVUploadRequest(BaseModel):
+    username: str
+    track_ids: list[str]
--- backend/app/routers/songs.py
+++ backend/app/routers/songs.py
@@ -1,23 +1,23 @@
 from fastapi import APIRouter, HTTPException
-from .. import crud
-from .. import utils
+from .. import crud, utils, models
+
 
 router = APIRouter()
 
 
 @router.post("/signup")
-def signup(username: str, password: str):
-    hashed_password = utils.hash_password(password)
-    user = crud.create_user(username, hashed_password)
+def signup(user: models.User):
+    hashed_password = utils.hash_password(user.password)
+    user = crud.create_user(user.username, hashed_password)
     if not user:
         raise HTTPException(status_code=400, detail="Username already registered")
     return user
 
 
 @router.post("/login")
-def login(username: str, password: str):
-    user = crud.authenticate_user(username, password)
-    if not user or not utils.verify_password(password, user['hashed_password']):
+def login(user: models.User):
+    user = crud.authenticate_user(user.username, user.password)
+    if not user or not utils.verify_password(user.password, user['hashed_password']):
         raise HTTPException(status_code=400, detail="Incorrect username or password")
     return {"username": user['username']}
 
@@ -33,36 +33,36 @@ def get_dislikes(username: str):
 
 
 @router.post("/like/csv")
-def upload_csv(username: str, track_ids: list):
-    if not crud.upload_csv(username, track_ids):
+def upload_csv(request: models.CSVUploadRequest):
+    if not crud.upload_csv(request.username, request.track_ids):
         raise HTTPException(status_code=400, detail="User not found")
     return {"status": "200"}
 
 
 @router.post("/like")
-def add_like(username: str, track_id: str):
-    if not crud.add_like(username, track_id):
+def add_like(request: models.UserRequest):
+    if not crud.add_like(request.username, request.track_id):
         raise HTTPException(status_code=400, detail="Track not found")
     return {"status": "200"}
 
 
 @router.post("/dislike")
-def add_dislike(username: str, track_id: str):
-    if not crud.add_dislike(username, track_id):
+def add_dislike(request: models.UserRequest):
+    if not crud.add_dislike(request.username, request.track_id):
         raise HTTPException(status_code=400, detail="Track not found")
     return {"status": "200"}
 
 
 @router.delete("/like")
-def remove_like(username: str, track_id: str):
-    if not crud.remove_like(username, track_id):
+def remove_like(request: models.UserRequest):
+    if not crud.remove_like(request.username, request.track_id):
         raise HTTPException(status_code=400, detail="Track not found")
     return {"status": "200"}
 
 
 @router.delete("/dislike")
-def remove_dislike(username: str, track_id: str):
-    if not crud.remove_dislike(username, track_id):
+def remove_dislike(request: models.UserRequest):
+    if not crud.remove_dislike(request.username, request.track_id):
         raise HTTPException(status_code=400, detail="Track not found")
     return {"status": "200"}
 
