b32b0a0c0b0142dc5fecaf37064441298e272c4d
Haim <Haim.Michalashvili@lusha.com>
2024-05-20 00:55:52 +0300

    - fix imports

--- backend/app/routers/songs.py
+++ backend/app/routers/songs.py
@@ -1,8 +1,8 @@
 from typing import List
 
 from fastapi import APIRouter, HTTPException
+from .. import crud
 from ..utils import hash_password, verify_password
-from ..crud import create_user, authenticate_user, get_likes, get_dislikes, upload_csv, add_like, add_dislike, remove_like, remove_dislike, get_recommendations
 from ..models import Track, User, UserTrackRequest, CSVUploadRequest
 
 router = APIRouter()
@@ -11,7 +11,7 @@ router = APIRouter()
 @router.post("/signup")
 def signup(user: User):
     hashed_password = hash_password(user.password)
-    user = create_user(user.username, hashed_password)
+    user = crud.create_user(user.username, hashed_password)
     if not user:
         raise HTTPException(status_code=400, detail="Username already registered")
     return user
@@ -19,7 +19,7 @@ def signup(user: User):
 
 @router.post("/login")
 def login(user: User):
-    user = authenticate_user(user.username, user.password)
+    user = crud.authenticate_user(user.username, user.password)
     if not user or not verify_password(user.password, user['hashed_password']):
         raise HTTPException(status_code=400, detail="Incorrect username or password")
     return {"username": user['username']}
@@ -27,49 +27,49 @@ def login(user: User):
 
 @router.get("/like")
 def get_likes(username: str):
-    return get_likes(username)
+    return crud.get_likes(username)
 
 
 @router.get("/dislike")
 def get_dislikes(username: str):
-    return get_dislikes(username)
+    return crud.get_dislikes(username)
 
 
 @router.post("/like/csv")
 def upload_csv(request: CSVUploadRequest):
-    if not upload_csv(request.username, request.track_ids):
+    if not crud.upload_csv(request.username, request.track_ids):
         raise HTTPException(status_code=400, detail="User not found")
     return {"status": "200"}
 
 
 @router.post("/like")
 def add_like(request: UserTrackRequest):
-    if not add_like(request.username, request.track_id):
+    if not crud.add_like(request.username, request.track_id):
         raise HTTPException(status_code=400, detail="Track not found")
     return {"status": "200"}
 
 
 @router.post("/dislike")
 def add_dislike(request: UserTrackRequest):
-    if not add_dislike(request.username, request.track_id):
+    if not crud.add_dislike(request.username, request.track_id):
         raise HTTPException(status_code=400, detail="Track not found")
     return {"status": "200"}
 
 
 @router.delete("/like")
 def remove_like(request: UserTrackRequest):
-    if not remove_like(request.username, request.track_id):
+    if not crud.remove_like(request.username, request.track_id):
         raise HTTPException(status_code=400, detail="Track not found")
     return {"status": "200"}
 
 
 @router.delete("/dislike")
 def remove_dislike(request: UserTrackRequest):
-    if not remove_dislike(request.username, request.track_id):
+    if not crud.remove_dislike(request.username, request.track_id):
         raise HTTPException(status_code=400, detail="Track not found")
     return {"status": "200"}
 
 
 @router.get("/recommendation", response_model=List[Track])
 def get_recommendations():
-    return get_recommendations()
+    return crud.get_recommendations()
