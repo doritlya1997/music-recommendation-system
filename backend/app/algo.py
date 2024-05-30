4c2f2e5886d12193ad572d12c1ad81a718c6fbb7
dorit.lya <dorit.lya@gmail.com>
2024-05-30 23:37:29 +0300

    deleted current path print

--- backend/app/algo.py
+++ backend/app/algo.py
@@ -87,9 +87,6 @@ def get_recommendations_by_user_listening_history(user_id: int):
                            'update_timestamp']
     other_cols = ['artist_name', 'duration_ms', 'genre', 'id', 'key', 'year', 'time_signature', 'track_id', 'track_name']
 
-    print("CURRENT PATH:")
-    print(os.getcwd())
-
     tracks_df = pd.concat(map(partial(pd.read_parquet),
                               glob.glob("./scripts/data_ready_for_db_parquet/*.parquet")))
     tracks_similarity_df = tracks_df[cols_for_similarity[:-1]]
